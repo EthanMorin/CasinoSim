c635588fed95ad648df44ffbb0cdbd56f56cc2d9
eaheil <129311664+eaheil@users.noreply.github.com>
2024-02-29 18:32:46 -0700

    composable and some rough game logic

--- pages/roulette.vue
+++ pages/roulette.vue
@@ -1,3 +1,39 @@
+<script setup>
+let gameWon = false
+
+function gameLoop() {
+  // 1. Get the user's bet
+  // 2. Spin the wheel
+  // 3. Check if the user won
+  // 4. Update the user's balance
+  // 5. Repeat
+  while (!gameWon) {
+    getBet()
+    spinWheel()
+    checkWin()
+    updateBalance()
+  }
+}
+
+function getBet() {
+
+}
+
+function spinWheel() {
+
+}
+
+function checkWin() {
+
+}
+
+function updateBalance() {
+
+}
+
+gameLoop()
+
+</script>
 <template>
   <h1>roulette</h1>
 </template>
