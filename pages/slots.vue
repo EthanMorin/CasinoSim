aba1a7a2031a618e2cfc904139be3ff490e95963
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-03-01 01:47:59 -0700

    fix: slot icons to slots assets

--- pages/slots.vue
+++ pages/slots.vue
@@ -1,8 +1,21 @@
-<script setup lang="js">
+<script setup></script>
 
-</script>
 <template>
-  
+  <div class="wheels">
+    <div class="wheel">
+      {/* Display wheel result */}
+    </div>
+    <div class="wheel">
+      {/* Display wheel result */}
+    </div>
+    <div class="wheel">
+      {/* Display wheel result */}
+    </div>
+  </div>
+  <div class="controls">
+    <p>5 Coins</p>
+    <button>Spin</button>
+  </div>
 </template>
 <style>
   
