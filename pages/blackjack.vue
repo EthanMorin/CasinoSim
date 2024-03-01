3bdb5e45333e0824e4329c1252355ca2511d4f75
paden <little.paden@gmail.com>
2024-02-29 19:00:20 -0700

    commit for new image names

--- pages/blackjack.vue
+++ pages/blackjack.vue
@@ -1,3 +1,14 @@
 <template>
   <h1>black jack</h1>
+  <board></board>
 </template>
+
+<script setup>
+import Board from "../composables/board.vue";
+</script>
+
+<style>
+body{
+  background-image: url('../public/Generic Felt Mat.jpeg')
+}
+</style>
