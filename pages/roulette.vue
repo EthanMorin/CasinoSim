a0011f8e26af12c87af042ccbed3048631a8d0a6
RatRyan <ryantratajczak@gmail.com>
2024-03-07 14:00:08 -0700

    feat: start betting

--- pages/roulette.vue
+++ pages/roulette.vue
@@ -1,7 +1,21 @@
 <script setup>
+const bet = reactive({
+  chips: [],
+});
+
 const rowThree = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36];
 const rowTwo = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35];
 const rowOne = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
+const redNumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+
+function placeChip(num) {
+  const chip = {
+    number: num,
+    amount: 5,
+  };
+  bet.chips.push(chip);
+  console.log(bet);
+}
 </script>
 
 <template>
@@ -14,17 +28,17 @@ const rowOne = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34];
       </div>
       <div class="board">
         <div class="row">
-          <div v-for="num in rowThree" class="space" @click="console.log(num)">
+          <div v-for="num in rowThree" class="space" @click="placeChip(num)">
             {{ num }}
           </div>
         </div>
         <div class="row">
-          <div v-for="num in rowTwo" class="space" @click="console.log(num)">
+          <div v-for="num in rowTwo" class="space" @click="placeChip(num)">
             {{ num }}
           </div>
         </div>
         <div class="row">
-          <div v-for="num in rowOne" class="space" @click="console.log(num)">
+          <div v-for="num in rowOne" class="space" @click="placeChip(num)">
             {{ num }}
           </div>
         </div>
