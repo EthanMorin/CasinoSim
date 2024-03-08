83065be9e7c0b9846551bbeb8446c2ef2e9be4f7
CamPartridge <cpartridge@student.neumont.edu>
2024-03-07 18:19:54 -0700

    wip

--- pages/bank.vue
+++ pages/bank.vue
@@ -5,16 +5,25 @@ const loanBalance = ref(loans)
 const currentBalance = ref(playerBalance)
 console.log(playerBalance)
 
+var inputMoney = ref(0);
+
+function Withdrawl(){
+    console.log(inputMoney)
+}
+
+function Deposit(){
+    
+}
 
 </script>
 <template>
-    <h1>bank</h1>
+    <h1>SimBank</h1>
     <div class="loans" v-text="loanBalance"></div>
     <div class="balance" v-text="currentBalance">
     </div>
     <div class="controls">
-        <input id="moneyBox" type="number">
-        <button class="button" id="withdrawl">Withdrawl</button>
-        <button class="button" id="deposit">Deposit</button>
+        <input v-text="inputMoney" id="moneyBox" type="number">
+        <button class="button" id="withdrawl" @click="Withdrawl()"()>Withdrawl</button>
+        <button class="button" id="deposit" @click="Deposit()">Deposit</button>
     </div>
 </template>
