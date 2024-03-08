0c99f5a07e23f621ebd41c7ea8d59ea64217fd6b
EthanMorin <119540016+EthanMorin@users.noreply.github.com>
2024-03-07 18:19:27 -0700

    update slots styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,39 @@
 <template>
 	<div class="game-display">
 		<div class="game-panel">
-			<NuxtLink to="/slots">
-				<div class="title-wrapper">
-					<h1>Slots</h1>
-				</div>
-				<img src="/public/card-back.png" alt="image of slots" />
-			</NuxtLink>
+			<div class="game-panel-content">
+				<NuxtLink to="/slots">
+					<div class="title-wrapper">
+						<h1>Slots</h1>
+					</div>
+					<img src="/public/card-back.png" alt="image of slots" />
+				</NuxtLink>
+			</div>
 		</div>
 		<div class="game-panel">
-			<NuxtLink to="/blackjack">
-				<div class="title-wrapper">
-					<h1>Black Jack</h1>
-				</div>
-				<img src="/public/card-back.png" alt="image of blackjack" />
-			</NuxtLink>
+			<div class="game-panel-content">
+				<NuxtLink to="/blackjack">
+					<div class="title-wrapper">
+						<h1>Black Jack</h1>
+					</div>
+					<img src="/public/card-back.png" alt="image of blackjack" />
+				</NuxtLink>
+			</div>
 		</div>
 		<div class="game-panel">
-			<NuxtLink to="/roulette">
-				<div class="title-wrapper">
-					<h1>Roulette</h1>
-				</div>
-				<img src="/public/card-back.png" alt="image of roulette" />
-			</NuxtLink>
+			<div class="game-panel-content">
+				<NuxtLink to="/roulette">
+					<div class="title-wrapper">
+						<h1>Roulette</h1>
+					</div>
+					<img src="/public/card-back.png" alt="image of roulette" />
+				</NuxtLink>
+			</div>
 		</div>
 	</div>
 </template>
 
 <style scoped>
-a {
-	text-decoration: none;
-	color: #F91E2E;
-}
-
 .game-display {
 	display: flex;
 	align-items: center;
@@ -52,11 +53,22 @@ a {
 	text-align: center;
 }
 
-/* Make fit the whole of the .game-panel background */
-.title-wrapper {
-	margin-top: -2%;
+.game-panel-content {
 	width: 100%;
-	border: solid 2px black;
-	margin-bottom: 10%;
+	a {
+		text-decoration: none;
+		color: #f91e2e;
+	}
+
+	.title-wrapper {
+		width: 100%;
+		border-bottom: solid 2px black;
+		margin-bottom: 10%;
+	}
+
+	img {
+		/* Fit to the card */
+		/* width: 100%; */
+	}
 }
 </style>
